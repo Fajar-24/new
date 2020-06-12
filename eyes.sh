33380183d257a01497a3a194475b5104e1acc5cc
https://github.com/Fajar-24 <65275087+Fajar-24@users.noreply.github.com>
2020-06-13 05:08:20 +0700

    Create eyes.sh

---
+++ eyes.sh
@@ -0,0 +1,202 @@
+
+clear
+figlet "TOOLS"
+echo "--------------------king-----------------"
+echo "KAMU HARUS INSTALL BAHAN NYA DULU"|lolcat
+echo "BY : CYBER EROR"|lolcat
+echo "--------------------king-----------------"
+echo "INSTALL BAHAN"|lolcat
+echo
+read -p "YA/TDK : " read tr
+echo "[*]WAIT"
+pkg upgrade && pkg update 
+pkg install figlet
+pkg install cowsay
+pkg install python2
+pip2 install lolcat
+clear
+
+figlet "LOGIN"|lolcat
+echo "SILAKAN LOGIN TOOLS DULU"|lolcat
+echo "USERNAME : CYBER "|lolcat
+echo "PASSWORD : TOOLS "|lolcat
+echo "SILAKAN LOGIN TOOLS DULU"|lolcat
+echo
+echo "1.[✓]LOGIN TOOLS"|lolcat
+read -p "pilih : " read tr
+
+if [ $pil-1 ]
+then
+clear
+figlet "LOGIN"|lolcat
+echo "USERNAME & TOOLS DI GABUNGKAN"|lolcat
+echo "JADI = CYBER TOOLS"|lolcat
+echo
+read -p "[✓]Username && Password : " read tr
+echo "[×]BERHASIL"
+clear
+fi
+
+#!/usr/bin/sh
+# coding=utf-8
+clear
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+clear
+cowsay -f eyes|lolcat
+echo "•×•×•×•×•×•×•×•×•×•×•×•×•×•÷•×•÷•÷•÷•×•×•"|lolcat
+echo "Author : FajarCyber"|lolcat
+echo "Kontak : 08817805411"|lolcat
+echo "Github : https://github.com/Fajar-24"|lolcat
+echo "••••••••••••••••••••••••••••••••••••••"|lolcat
+echo  " JANGAN DI SALAH GUNAKAN TOD "|lolcat
+figlet "EROR"|lolcat
+echo "•×•×•×•×•×•×•×•×•×•×•×•×•×•×•×•×•×•×•×•××"|lolcat
+echo "Untuk Username Dan Password Hubungi 08817805411"|lolcat
+echo "GUNAKAN TOOLS INI DENGAN BIJAK"|lolcat
+echo "pilih menu Tools nya"|lolcat
+echo
+echo $i"────────────────────────────────    ─────"
+echo $i"|"$me" 1"$i" |"$cy" DARK NEW        "$i"|"$i"    |"$cy" B"$i" |"|lolcat
+echo $i"|"$me" 2"$i" |"$cy" PHISING     "$i"|"$i"    |"$cy" E"$i" |"|lolcat
+echo $i"|"$me" 3"$i" |"$cy" MBF      "$i"|"$i"    |"$cy" N"$i" |"|lolcat
+echo $i"|"$me" 4"$i" |"$cy" Hack FREE FIRE      "$i"|"$i"    |"$cy" N"$i" |"|lolcat
+echo $i"|"$me" 5"$i" |"$cy" HACK PULSA      "$i"|"$i"    |"$cy" N"$i" |"
+echo $i"|"$me" 6"$i" |"$cy" HACK WHATSAPP      "$i"|"$i"    |"$cy" N"$i" |"|lolcat
+echo $i"|"$me" 7"$i" |"$cy" HACK FB      "$i"|"$i"    |"$cy" N"$i" |"|lolcat
+echo $i"|"$me" 8"$i" |"$cy" SPAMERR      "$i"|"$i"    |"$cy" N"$i" |"|lolcat
+echo $i"|"$me" 99"$i" |"$cy" EXIT      "$i"|"$i"    |"$cy" N"$i" |"|lolcat
+echo $i"────────────────────────────────"$i"    |"$cy" L"$i" |"|lolcat
+echo 
+read -p"└──# " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "B A C O T"|lolcat
+sleep 1
+git clone https://github.com/Fajar-24/CyberNEW
+cd CyberNEW
+python2 Cyber.py
+fi
+
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "B A C O T"|lolcat
+sleep 1
+git clone https://github.com/Fajar-24/phising
+cd phising
+sh v2.sh
+fi
+
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "B A C O T"|lolcat
+sleep 1
+git clone https://github.com/Fajar-24/CendolDawet
+cd CendolDawet
+python2 Hack.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "B A C O T"|lolcat
+sleep 1
+echo " TUNGGU SEBENTAR SCRIPT AKAN DI INSTALL "|lolcat
+echo " SCRIPT AKAN DI INSTALL TUNGGU 5 DETIK "|lolcat
+figlet "5"|lolcat
+figlet "4"|lolcat
+figlet "3"|lolcat
+figlet "2"|lolcat
+figlet "1"|lolcat
+clear
+git clone https://github.com/Fajar-24/FreeFire
+cd FreeFire
+sh Fire.sh
+fi
+
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "B A C O T"|lolcat
+sleep 1
+echo " TUNGGU SEBENTAR SCRIPT AKAN DI INSTALL "|lolcat
+echo " SCRIPT AKAN DI INSTALL TUNGGU 5 DETIK "|lolcat
+figlet "5"|lolcat
+figlet "4"|lolcat
+figlet "3"|lolcat
+figlet "2"|lolcat
+figlet "1"|lolcat
+git clone https://github.com/Fajar-24/HACKPULSA
+cd HACKPULSA
+sh HACKPULSA.sh
+fi
+
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "B A C O T"|lolcat
+sleep 1
+echo " TUNGGU SEBENTAR SCRIPT AKAN DI INSTALL "|lolcat
+echo " SCRIPT AKAN DI INSTALL TUNGGU 5 DETIK "|lolcat
+figlet "5"|lolcat
+clear
+figlet "4"|lolcat
+clear
+figlet "3"|lolcat
+clear
+figlet "2"|lolcat
+clear
+figlet "1"|lolcat
+git clone https://github.com/Fajar-24/HACKWHATSAPP
+cd HACKWHATSAPP
+sh WhatSapp.sh
+fi
+
+if [ $pil = 7 ]
+then
+clear
+cowsay -f eyes "FAJAR CYBER"|lolcat
+echo "---------------king-------------"
+echo "TUNGGU SEBENTAR..."|lolcat
+git clone https://github.com/Fajar-24/Dark1.0
+cd Dark1.0
+sh Dark.sh
+fi
+
+if [ $pil = 8 ]
+then
+clear
+cowsay -f eyes "FAJAR CYBER"|lolcat
+echo "---------------king-------------"
+echo "TUNGGU SEBENTAR... "|lolcat
+git clone https://github.com/Fajar-24/CYBER.eror
+cd CYBER.eror
+sh cyber.sh
+fi
+
+if [ $pil = 99 ]
+then
+echo "TERIMA KASIH TELAH MENGGUNAKAN TOOLS INI"|lolcat
+echo "THANKS|lolcat
+echo "BY : FAJAR CYBER"|lolcat
